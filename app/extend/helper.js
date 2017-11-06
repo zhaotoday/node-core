2897a14c2497bad21808321d4105c6686d30e314
zhaotoday <729234283@qq.com>
2017-11-06 23:29:27 +0800

    新增分页

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -40,5 +40,33 @@ module.exports = {
    */
   md5 (string) {
     return crypto.createHash('md5').update(string).digest('hex')
+  },
+
+  /**
+   * 分页
+   * @returns {string}
+   */
+  page ({url = '', total, offset = 0, limit} = {}) {
+    const pages = Math.ceil(total / limit)
+
+    let ret = `<div class="page">`
+
+    if (offset === 0) {
+      ret += `<span class="page-first">首页</span>`
+      ret += `<span class="page-prev">上一页</span>`
+    } else {
+      ret += `<a class="page-first" href="${url}?offset=0">首页</a>`
+      ret += `<a class="page-prev" href="${url}?offset=${offset - limit}">上一页</a>`
+    }
+
+    if (pages === offset / limit + 1) {
+      ret += `<span class="page-next">下一页</span>`
+      ret += `<span class="page-last">尾页</span>`
+    } else {
+      ret += `<a class="page-next" href="${url}?offset=${offset + limit}">下一页</a>`
+      ret += `<a class="page-last" href="${url}?offset=${(pages - 1) * limit}">尾页</a>`
+    }
+
+    return ret
   }
 }
