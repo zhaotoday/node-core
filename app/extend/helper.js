6a0d39a849863938f29ff52d3861122009529db3
zhaotoday <729234283@qq.com>
2017-11-07 23:23:42 +0800

    BUG 修复

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -14,7 +14,7 @@ module.exports = {
    * @returns {Object}
    */
   formatQuery (query) {
-    const {offset = 0, limit = 10, where = '', order = [['id', 'DESC']]} = query
+    const {offset = 0, limit = 10, where = '{}', order = [['id', 'DESC']]} = query
 
     return {offset: +offset, limit: +limit, where: JSON.parse(where), order}
   },
@@ -31,28 +31,28 @@ module.exports = {
    * 分页
    * @returns {string}
    */
-  page ({url = '', total, offset = 0, limit} = {}) {
+  page ({url = '', total, page = 1, limit} = {}) {
     const pages = Math.ceil(total / limit)
 
     let ret = `<div class="page">`
 
-    if (offset === 0) {
+    if (page === 1) {
       ret += `<span class="page-first">首页</span>`
       ret += `<span class="page-prev">上一页</span>`
     } else {
-      ret += `<a class="page-first" href="${url}?offset=0">首页</a>`
-      ret += `<a class="page-prev" href="${url}?offset=${offset - limit}">上一页</a>`
+      ret += `<a class="page-first" href="${url}?p=1">首页</a>`
+      ret += `<a class="page-prev" href="${url}?p=${page - 1}">上一页</a>`
     }
 
-    if (pages === offset / limit + 1) {
+    if (pages === page) {
       ret += `<span class="page-next">下一页</span>`
       ret += `<span class="page-last">尾页</span>`
     } else {
-      ret += `<a class="page-next" href="${url}?offset=${offset + limit}">下一页</a>`
-      ret += `<a class="page-last" href="${url}?offset=${(pages - 1) * limit}">尾页</a>`
+      ret += `<a class="page-next" href="${url}?p=${page + 1}">下一页</a>`
+      ret += `<a class="page-last" href="${url}?p=${pages}">尾页</a>`
     }
 
-    ret = `</div>`
+    ret += `</div>`
 
     return ret
   }
